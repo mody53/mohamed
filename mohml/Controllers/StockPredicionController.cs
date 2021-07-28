6a9e05b2c4a4f4d4074389570258101b62ccfdf7
mody53 <m15150987@yahoo.com>
2021-07-28 16:44:22 +0500

    c1

---
+++ mohml/Controllers/StockPredicionController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MohmlML.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mohml.Controllers
+{
+    public class StockPredictionController : Controller
+    {
+        [HttpGet]
+        public IActionResult StockPrediction()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult StockPrediction(ModelInput input)
+        {
+            ViewBag.Result = "";
+            var stockPredictions = ConsumeModel.Predict(input);
+            ViewBag.Result = stockPredictions;
+
+            ViewData["ItemID"] = input.ItemID;
+            ViewData["Loccode"] = input.Loccode;
+            return View();
+        }
+    }
+}
